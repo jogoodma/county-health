994ce2dcefd28113477727bfc09dcbc5a4991599
Josh Goodman <jogoodma@iu.edu>
2022-11-20 14:11:29 -0500

    Add Astro deployment step to GH pages

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jogoodma.github.io',
+  base: '/county-health',
   integrations: [react(), tailwind()]
 });
