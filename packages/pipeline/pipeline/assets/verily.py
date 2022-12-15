936664cf686d9cb1fdd2342f39ba1249037d0646
Josh Goodman <jogoodma@iu.edu>
2022-12-15 15:45:27 -0500

    Fix several issues with Verily FIPS codes
    Also upgrade DuckDB python and nodejs libraries
    to v0.6.1

--- packages/pipeline/pipeline/assets/verily.py
+++ packages/pipeline/pipeline/assets/verily.py
@@ -7,8 +7,25 @@ from ..utils.sql import SQL
 def verily_wastewater() -> SQL:
     df = pd.read_csv("https://publichealth.verily.com/api/csv")
 
-    # Remove the row that has Alameda in the County_FIPS column.
+    # Several fixes for the 'County_FIPS' column.
+    # Replace the county name with the FIPS code.
     df["County_FIPS"] = df["County_FIPS"].replace("Alameda", "06001")
+    df["County_FIPS"] = df["County_FIPS"].replace("Stanislaus", "06099")
+    df["County_FIPS"] = df["County_FIPS"].replace("Yolo", "06113")
+
+    # Look for rows with multiple FIPS codes in a single cell.
+    county_fips_with_multiple_counties = df["County_FIPS"].str.contains(",", na=False)
+    # Copy them into a new dataframe.
+    multi_fips_rows = df[county_fips_with_multiple_counties]
+    # Remove them.
+    df.drop(multi_fips_rows.index, inplace=True)
+
+    # Split the FIPS codes into separate rows and reinsert them into the dataframe.
+    for idx, row in multi_fips_rows.iterrows():
+        for fips in row["County_FIPS"].split(","):
+            df = pd.concat([df, row], ignore_index=True)
+            df.iloc[-1, df.columns.get_loc("County_FIPS")] = fips
+
     return SQL("SELECT * FROM $df", df=df)
 
 
